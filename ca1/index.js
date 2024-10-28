8b614144af060909b885f25f953a8d4d9ea750a1
Ciaran Cruz <ciarancaesarcruz@gmail.com>
2024-10-28 21:41:06 +0000

    added firestore and displaying list of existing saves

--- ca1/index.js
+++ ca1/index.js
@@ -1,8 +1,30 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { doc, collection, getDocs, addDoc, setDoc, getDoc, getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAJVhSTIT-pUOuOpWkhsaN5dZ5mjh-AIoI",
+  authDomain: "rwat-ca1.firebaseapp.com",
+  projectId: "rwat-ca1",
+  storageBucket: "rwat-ca1.appspot.com",
+  messagingSenderId: "765014458593",
+  appId: "1:765014458593:web:35733708a4e1de2506f7a0",
+  measurementId: "G-PMBKE224GH"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+
 ///// Initialize buttons /////
 const startNewButton = document.getElementById("startNewButton");
 startNewButton.addEventListener("click", startNewPicture);
 const retrieveExistingButton = document.getElementById("retrieveExistingButton");
-// retrieveExistingButton.addEventListener("click", );
+retrieveExistingButton.addEventListener("click", loadExistingGame);
 
 
 ///// Show or hide screens /////
@@ -10,18 +32,28 @@ function showEntryScreen() {
     document.getElementById('entryScreen').style.display = "block";
     document.getElementById('specifySizeScreen').style.display = "none";
     document.getElementById('pictureScreen').style.display = "none";
+    document.getElementById('existingSaveScreen').style.display = "none";
 }
 
 function showSpecifySizeScreen() {
     document.getElementById('specifySizeScreen').style.display = "block";
     document.getElementById('entryScreen').style.display = "none";
     document.getElementById('pictureScreen').style.display = "none";
+    document.getElementById('existingSaveScreen').style.display = "none";
 }
 
 function showPictureScreen() {
     document.getElementById('pictureScreen').style.display = "block";
     document.getElementById('entryScreen').style.display = "none";
     document.getElementById('specifySizeScreen').style.display = "none";
+    document.getElementById('existingSaveScreen').style.display = "none";
+}
+
+function showSaveScreen() {
+    document.getElementById('existingSaveScreen').style.display = "block";
+    document.getElementById('entryScreen').style.display = "none";
+    document.getElementById('specifySizeScreen').style.display = "none";
+    document.getElementById('pictureScreen').style.display = "none";
 }
 
 ///// Initialize screens /////
@@ -109,3 +141,48 @@ function addTextBox(finalBox) {
     finalBox.innerHTML = `<textarea></textarea>`;
 }
 
+
+///// Save and Retrieve Functionality ///// 
+
+// Save button event listener
+const savePictureButton = document.getElementById("savePicture");
+savePictureButton.addEventListener("click", savePictureGrid);
+
+// Load existing game
+async function loadExistingGame() {
+    const drawingSaves = await getDocs(collection(db, "drawingSaves"));
+
+    // Display list of existing saves
+    const tableBody = document.getElementById("listSaves").getElementsByTagName('tbody')[0];
+    drawingSaves.forEach((doc) => {
+        const row = document.createElement("tr");
+        const id = document.createElement("td");
+        const button = document.createElement("td");
+
+        id.innerHTML = doc.id;
+
+        const playButton = document.createElement("button");
+        playButton.id = `${doc.id}button`;
+        playButton.innerHTML = "Play";
+        playButton.addEventListener("click", drag);
+
+        button.appendChild(playButton);
+        row.appendChild(id);
+        row.appendChild(button);
+
+        tableBody.appendChild(row);
+    });
+
+    showSaveScreen();
+}
+
+// Add a save to firestore db
+function savePictureGrid() {
+    
+    // Save arrows
+
+
+    showEntryScreen();
+
+
+}
