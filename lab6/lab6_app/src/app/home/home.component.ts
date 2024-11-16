0bbffb094e29e058daf19b0e8e8af575e1a1546e
Ciaran Cruz <ciarancaesarcruz@gmail.com>
2024-11-16 16:05:50 +0000

    added angular service

--- lab6/lab6_app/src/app/home/home.component.ts
+++ lab6/lab6_app/src/app/home/home.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RecipesComponent } from '../recipes/recipes.component';
 import { CommonModule } from '@angular/common';
 import { Recipe } from '../recipe';
+import { FoodRecipesService } from '../food-recipes.service';
 
 @Component({
   selector: 'app-home',
@@ -12,71 +13,10 @@ import { Recipe } from '../recipe';
 })
 
 export class HomeComponent {
-  
-  recipeList: Recipe[] = [
-    {
-      id: 1,
-      recipeName: "Carbonara",
-      description: "A creamy pasta dish with eggs, cheese, pancetta, and pepper.",
-      servings: 4,
-      cusineType: "Italian",
-      favourite: true
-    },
-    {
-      id: 2,
-      recipeName: "Chicken Curry",
-      description: "A spicy and flavorful chicken curry with aromatic spices.",
-      servings: 6,
-      cusineType: "Indian",
-      favourite: false
-    },
-    {
-      id: 3,
-      recipeName: "Sushi",
-      description: "Japanese dish made with vinegared rice, seafood, and vegetables.",
-      servings: 2,
-      cusineType: "Japanese",
-      favourite: true
-    },
-    {
-      id: 4,
-      recipeName: "Tacos",
-      description: "Mexican dish consisting of a folded or rolled tortilla filled with various fillings.",
-      servings: 4,
-      cusineType: "Mexican",
-      favourite: false
-    },
-    {
-      id: 5,
-      recipeName: "Caesar Salad",
-      description: "A classic salad with romaine lettuce, croutons, Parmesan, and Caesar dressing.",
-      servings: 2,
-      cusineType: "American",
-      favourite: true
-    },
-    {
-      id: 6,
-      recipeName: "Vegetable Stir-fry",
-      description: "A healthy stir-fry with mixed vegetables, soy sauce, and sesame oil.",
-      servings: 3,
-      cusineType: "Chinese",
-      favourite: false
-    },
-    {
-      id: 7,
-      recipeName: "Beef Stroganoff",
-      description: "A rich and creamy beef dish served over noodles or rice.",
-      servings: 5,
-      cusineType: "Russian",
-      favourite: true
-    },
-    {
-      id: 8,
-      recipeName: "Falafel",
-      description: "Deep-fried balls made from chickpeas and herbs, often served in pita bread.",
-      servings: 4,
-      cusineType: "Middle Eastern",
-      favourite: false
-    }
-  ];
+  foodRecipeService: FoodRecipesService = inject(FoodRecipesService);
+
+  recipeList: Recipe[] = [];
+  constructor() {
+    this.recipeList = this.foodRecipeService.getAllRecipes();
+  }
 }
